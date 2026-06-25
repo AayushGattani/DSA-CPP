0fe8629cf4123a5459b3318cdeaeeaba0065d474
AayushGattani <aayushgattani98@gmail.com>
2026-06-25 13:49:26 +0530

    Time: 8 ms (37.76%) | Memory: 13.5 MB (90.50%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res="";
+        for(int i=0;i<s.length();i++){
+            if(!res.empty() && s[i]==res.back()){
+                res.pop_back();
+            }
+            else{
+                res.push_back(s[i]);
+            }
+        }
+        return res;
+
+        
+    }
+};
